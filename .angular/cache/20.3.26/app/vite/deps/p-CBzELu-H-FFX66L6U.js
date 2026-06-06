3e60e5ca828fe2f0c09c6e712305b10b8ab649b6
afonsogomes1417 <ironenderman500@gmail.com>
2026-06-06 19:13:09 +0100

    tarefa dificil + apk

---
+++ .angular/cache/20.3.26/app/vite/deps/p-CBzELu-H-FFX66L6U.js
@@ -0,0 +1,13 @@
+import {
+  a,
+  l
+} from "./chunk-7AVYKGIZ.js";
+import "./chunk-2S2P52YH.js";
+import "./chunk-4QUALYAX.js";
+import "./chunk-UH32AR35.js";
+import "./chunk-FBOO75ZN.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  l as iosTransitionAnimation,
+  a as shadow
+};
---
+++ .angular/cache/20.3.26/app/vite/deps/p-CU1SSH8_-EA5HIWHL.js
@@ -0,0 +1,11 @@
+import {
+  i
+} from "./chunk-7GU4EKU3.js";
+import "./chunk-2S2P52YH.js";
+import "./chunk-4QUALYAX.js";
+import "./chunk-UH32AR35.js";
+import "./chunk-FBOO75ZN.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  i as mdTransitionAnimation
+};
